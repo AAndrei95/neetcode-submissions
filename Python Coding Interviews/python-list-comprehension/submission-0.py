7ab35f9c589e140befaab6a8779acf163434f80e
Andrei Alexandru <135723402+AAndrei95@users.noreply.github.com>
2026-07-27 12:18:25 +0100

    Add: python-list-comprehension - submission-0

---
+++ Python Coding Interviews/python-list-comprehension/submission-0.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def create_list_of_odds(n: int) -> List[int]:
+    odd_nums = [num for num in range(1, n+1) if num%2 != 0]
+    return odd_nums
+
+# do not modify below this line
+print(create_list_of_odds(1))
+print(create_list_of_odds(5))
+print(create_list_of_odds(6))
+print(create_list_of_odds(10))
